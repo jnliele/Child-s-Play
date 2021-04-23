30ddc59185e3b0300d32076b4d255547a61b9b6b
Tony-Kha <tony.kha@mavs.uta.edu>
2021-04-23 01:52:13 -0500

    alphabet game levels added

---
+++ app/src/main/java/com/example/teachingtoddlers/alphabetGameLvl3.java
@@ -0,0 +1,156 @@
+package com.example.teachingtoddlers;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class alphabetGameLvl3 extends AppCompatActivity {
+
+    alphabetQuestions questions = new alphabetQuestions();
+
+    Button Ans1, Ans2, Ans3, Ans4;
+    TextView Score, Question;
+
+    String answer;
+    int score = 0;
+    int questionNum = questions.questionsLvl3.length;
+
+    Random r;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabet_game_lvl3);
+
+        r = new Random();
+
+        Ans1 = (Button) findViewById(R.id.Ans1);
+        Ans2 = (Button) findViewById(R.id.Ans2);
+        Ans3 = (Button) findViewById(R.id.Ans3);
+        Ans4 = (Button) findViewById(R.id.Ans4);
+
+        Question = (TextView) findViewById(R.id.Question);
+        Score = (TextView) findViewById(R.id.Score);
+        Score.setText("Score: " + score);
+
+        updateQuestion(r.nextInt(questionNum));
+
+        Ans1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Ans1.getText() == answer){
+                    score++;
+                    if(score == 10){
+                        gameEnd();
+                    }else{
+                        Score.setText("Score: " + score);
+                        updateQuestion(r.nextInt(questionNum));
+                        Toast.makeText(alphabetGameLvl3.this, "correct", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(alphabetGameLvl3.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion(r.nextInt(questionNum));
+                }
+            }
+        });
+
+        Ans2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Ans2.getText() == answer){
+                    score++;
+                    if(score == 10){
+                        gameEnd();
+                    }else{
+                        Score.setText("Score: " + score);
+                        updateQuestion(r.nextInt(questionNum));
+                        Toast.makeText(alphabetGameLvl3.this, "correct", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(alphabetGameLvl3.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion(r.nextInt(questionNum));
+                }
+            }
+        });
+
+        Ans3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Ans3.getText() == answer){
+                    score++;
+                    if(score == 10){
+                        gameEnd();
+                    }else{
+                        Score.setText("Score: " + score);
+                        updateQuestion(r.nextInt(questionNum));
+                        Toast.makeText(alphabetGameLvl3.this, "correct", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(alphabetGameLvl3.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion(r.nextInt(questionNum));
+                }
+            }
+        });
+
+        Ans4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Ans4.getText() == answer){
+                    score++;
+                    if(score == 10){
+                        gameEnd();
+                    }else{
+                        Score.setText("Score: " + score);
+                        updateQuestion(r.nextInt(questionNum));
+                        Toast.makeText(alphabetGameLvl3.this, "correct", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else{
+                    Toast.makeText(alphabetGameLvl3.this, "wrong", Toast.LENGTH_SHORT).show();
+                    updateQuestion(r.nextInt(questionNum));
+                }
+            }
+        });
+    }
+
+    private void gameEnd() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(alphabetGameLvl3.this);
+        alertDialogBuilder
+                .setMessage("You reached a score of 10!")
+                .setCancelable(false)
+                .setPositiveButton("Finish",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                startActivity(new Intent(getApplicationContext(), alphabetGame.class));
+                            }
+                        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void updateQuestion(int num) {
+        Question.setText(questions.getQuestionLvl3(num));
+
+        Ans1.setText(questions.getChoice1Lvl3(num));
+        Ans2.setText(questions.getChoice2Lvl3(num));
+        Ans3.setText(questions.getChoice3Lvl3(num));
+        Ans4.setText(questions.getChoice4Lvl3(num));
+
+        answer = questions.correctLvl3(num);
+    }
+}
--- app/src/main/java/com/example/teachingtoddlers/alphabetQuestions.java
+++ app/src/main/java/com/example/teachingtoddlers/alphabetQuestions.java
@@ -1,8 +1,37 @@
 package com.example.teachingtoddlers;
 
 public class alphabetQuestions {
-    
-    public String questions[] = {
+
+    public String questionsLvl1[] = {
+            "What letter goes in the blank\n_ b c d e",
+            "What letter goes in the blank\na _ c d e",
+            "What letter goes in the blank\na b _ d e",
+            "What letter goes in the blank\nb c _ e f",
+            "What letter goes in the blank\nc d _ f g",
+            "What letter goes in the blank\nd e _ g h",
+            "What letter goes in the blank\ne f _ h i",
+            "What letter goes in the blank\nf g _ i j",
+    };
+
+    public String questionsLvl2[] = {
+            "What letter goes in the blank\n_ b c d e",
+            "What letter goes in the blank\na _ c d e",
+            "What letter goes in the blank\na b _ d e",
+            "What letter goes in the blank\nb c _ e f",
+            "What letter goes in the blank\nc d _ f g",
+            "What letter goes in the blank\nd e _ g h",
+            "What letter goes in the blank\ne f _ h i",
+            "What letter goes in the blank\nf g _ i j",
+            "What letter goes in the blank\ng h _ j k",
+            "What letter goes in the blank\nh i _ k l",
+            "What letter goes in the blank\ni j _ l m",
+            "What letter goes in the blank\nj k _ m n",
+            "What letter goes in the blank\nk l _ n o",
+            "What letter goes in the blank\nl m _ o p",
+            "What letter goes in the blank\nm n _ p q",
+            "What letter goes in the blank\nn o _ q r"
+    };
+    public String questionsLvl3[] = {
             "What letter goes in the blank\n_ b c d e",
             "What letter goes in the blank\na _ c d e",
             "What letter goes in the blank\na b _ d e",
@@ -31,7 +60,37 @@ public class alphabetQuestions {
             "What letter goes in the blank\nv w x y _"
     };
 
-    private String choices[][] = {
+    private String choicesLvl1[][] = {
+            {"f", "a", "g", "e"},
+            {"e", "r", "w", "b"},
+            {"c", "t", "s", "f"},
+            {"h", "d", "g", "e"},
+            {"s", "e", "v", "c"},
+            {"k", "e", "d", "f"},
+            {"h", "y", "g", "q"},
+            {"s", "h", "r", "f"}
+    };
+
+    private String choicesLvl2[][] = {
+            {"f", "a", "g", "e"},
+            {"e", "r", "w", "b"},
+            {"c", "t", "s", "f"},
+            {"h", "d", "g", "e"},
+            {"s", "e", "v", "c"},
+            {"k", "e", "d", "f"},
+            {"h", "y", "g", "q"},
+            {"s", "h", "r", "f"},
+            {"y", "e", "i", "f"},
+            {"j", "y", "g", "n"},
+            {"n", "k", "f", "c"},
+            {"i", "j", "s", "l"},
+            {"i", "f", "e", "m"},
+            {"v", "j", "n", "y"},
+            {"o", "w", "h", "r"},
+            {"j", "p", "s", "f"}
+    };
+
+    private String choicesLvl3[][] = {
             {"f", "a", "g", "e"},
             {"e", "r", "w", "b"},
             {"c", "t", "s", "f"},
@@ -60,35 +119,101 @@ public class alphabetQuestions {
             {"f", "z", "k", "p"}
     };
 
-    private String answers[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"};
+    private String answersLvl1[] = {"a","b","c","d","e","f","g","h"};
+    private String answersLvl2[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"};
+    private String answersLvl3[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","w","y","z"};
 
-    public String getQuestion(int a){
-        String question = questions[a];
+    //Level 1
+    public String getQuestionLvl1(int a){
+        String question = questionsLvl1[a];
         return question;
     }
 
-    public String getChoice1(int a){
-        String choice = choices[a][0];
+    public String getChoice1Lvl1(int a){
+        String choice = choicesLvl1[a][0];
         return choice;
     }
 
-    public String getChoice2(int a){
-        String choice = choices[a][1];
+    public String getChoice2Lvl1(int a){
+        String choice = choicesLvl1[a][1];
         return choice;
     }
 
-    public String getChoice3(int a){
-        String choice = choices[a][2];
+    public String getChoice3Lvl1(int a){
+        String choice = choicesLvl1[a][2];
         return choice;
     }
 
-    public String getChoice4(int a){
-        String choice = choices[a][3];
+    public String getChoice4Lvl1(int a){
+        String choice = choicesLvl1[a][3];
         return choice;
     }
 
-    public String correct(int a){
-        String answer = answers[a];
+    public String correctLvl1(int a){
+        String answer = answersLvl1[a];
         return answer;
     }
+
+    //Level 2
+    public String getQuestionLvl2(int a){
+        String question = questionsLvl2[a];
+        return question;
+    }
+
+    public String getChoice1Lvl2(int a){
+        String choice = choicesLvl2[a][0];
+        return choice;
+    }
+
+    public String getChoice2Lvl2(int a){
+        String choice = choicesLvl2[a][1];
+        return choice;
+    }
+
+    public String getChoice3Lvl2(int a){
+        String choice = choicesLvl2[a][2];
+        return choice;
+    }
+
+    public String getChoice4Lvl2(int a){
+        String choice = choicesLvl2[a][3];
+        return choice;
+    }
+
+    public String correctLvl2(int a){
+        String answer = answersLvl2[a];
+        return answer;
+    }
+
+    //Level 3
+    public String getQuestionLvl3(int a){
+        String question = questionsLvl3[a];
+        return question;
+    }
+
+    public String getChoice1Lvl3(int a){
+        String choice = choicesLvl3[a][0];
+        return choice;
+    }
+
+    public String getChoice2Lvl3(int a){
+        String choice = choicesLvl3[a][1];
+        return choice;
+    }
+
+    public String getChoice3Lvl3(int a){
+        String choice = choicesLvl3[a][2];
+        return choice;
+    }
+
+    public String getChoice4Lvl3(int a){
+        String choice = choicesLvl3[a][3];
+        return choice;
+    }
+
+    public String correctLvl3(int a){
+        String answer = answersLvl3[a];
+        return answer;
+    }
+
 }
